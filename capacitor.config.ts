f87125d302b579cf5a22b20e8ac768d1ee86d9d5
Javier Sánchez <javier.sanchezc@campusfp.es>
2023-01-10 11:03:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CV_Ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
